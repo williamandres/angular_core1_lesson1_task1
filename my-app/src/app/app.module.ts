291972075aa520e23d7e0ec8b8e6cde3bfe9dad2
williamandres <williamaguilar@misena.edu.co>
2021-06-09 11:55:43 -0500

    creating first component ever

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuDrinksComponent } from './menu-drinks/menu-drinks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MenuComponent, MenuDrinksComponent 
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app_menu",
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
+
+})
+
+export class MenuComponent{
+    title_menu = "Menu comida mexicana"
+}
